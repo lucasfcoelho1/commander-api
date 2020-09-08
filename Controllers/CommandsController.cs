06e258a370872fa9f071e7872a507fc1b99ac7c6
Lucas Coelho <lucas.coelho@lambda3.com.br>
2020-09-08 10:44:23 -0300

    Initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo repository;
+
+        public CommandsController(ICommanderRepo repository) =>
+            this.repository = repository;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
